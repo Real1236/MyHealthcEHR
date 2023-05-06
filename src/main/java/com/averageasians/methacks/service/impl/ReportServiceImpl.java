444836f7cda182dffe4fcfbb33c82129afb5dba6
Arthur <arthurchan2001@live.com>
2023-05-06 11:57:05 -0400

    Configured WebClient

--- src/main/java/com/averageasians/methacks/service/impl/ReportServiceImpl.java
+++ src/main/java/com/averageasians/methacks/service/impl/ReportServiceImpl.java
@@ -5,6 +5,7 @@ import com.averageasians.methacks.entity.Report;
 import com.averageasians.methacks.service.ReportService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.List;
 import java.util.Optional;
@@ -13,10 +14,13 @@ import java.util.Optional;
 public class ReportServiceImpl implements ReportService {
 
     ReportDAO reportDAO;
+    WebClient webClient;
+    public final String bearer = "y6X5xcFGAI1ywywYx8EBo1vZdkYNeUbF2Qy15DTq";
 
     @Autowired
-    public ReportServiceImpl(ReportDAO reportDAO) {
+    public ReportServiceImpl(ReportDAO reportDAO, WebClient webClient) {
         this.reportDAO = reportDAO;
+        this.webClient = webClient;
     }
 
     @Override
