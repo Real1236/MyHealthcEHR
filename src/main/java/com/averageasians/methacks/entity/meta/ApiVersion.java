0f59d0b59c3bdf738745191b5e611ae792373a94
Arthur <arthurchan2001@live.com>
2023-05-06 12:16:48 -0400

    Created meta classes

---
+++ src/main/java/com/averageasians/methacks/entity/meta/ApiVersion.java
@@ -0,0 +1,10 @@
+package com.averageasians.methacks.entity.meta;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ApiVersion {
+    private String version;
+}
