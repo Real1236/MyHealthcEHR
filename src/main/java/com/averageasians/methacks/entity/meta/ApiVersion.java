95522580ad552bade7d36d8926e8874864c883ec
Arthur <arthurchan2001@live.com>
2023-05-06 14:04:52 -0400

    Added no arg constructor for meta

--- src/main/java/com/averageasians/methacks/entity/meta/ApiVersion.java
+++ src/main/java/com/averageasians/methacks/entity/meta/ApiVersion.java
@@ -2,8 +2,10 @@ package com.averageasians.methacks.entity.meta;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class ApiVersion {
     private String version;
