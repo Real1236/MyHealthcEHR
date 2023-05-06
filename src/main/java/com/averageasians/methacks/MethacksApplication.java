fc93dfdb4f1709ef1dd2cae51c63fb52b3c781a8
Arthur <arthurchan2001@live.com>
2023-05-05 20:45:04 -0400

    Initial commit

---
+++ src/main/java/com/averageasians/methacks/MethacksApplication.java
@@ -0,0 +1,13 @@
+package com.averageasians.methacks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MethacksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MethacksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/averageasians/methacks/MethacksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.averageasians.methacks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MethacksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
